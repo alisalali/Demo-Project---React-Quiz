ff2e94ebd992435d5a8f400768107b908e2d79ef
alisalali <alis.alali@hotmail.com>
2024-11-05 00:36:20 +0300

    05-working-with-effect-deps

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,34 +1,46 @@
-import { useState } from "react";
-import QUESTIONS from "../questions";
+import { useState, useCallback } from "react";
+
+import QUESTIONS from "../questions.js";
+import QuestionTimer from "./QuestionTimer.jsx";
 import quizCompleteImg from "../assets/quiz-complete.png";
 
-export default function () {
+export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
-  const activeQuestionsIndex = userAnswers.length;
-  const quizIsComplete = activeQuestionsIndex === QUESTIONS.length;
 
-  function handleSelectAnswer(selectedAnswer) {
+  const activeQuestionIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionIndex === QUESTIONS.length;
+
+  const handleSelectAnswer = useCallback(function handleSelectAnswer(
+    selectedAnswer
+  ) {
     setUserAnswers((prevUserAnswers) => {
       return [...prevUserAnswers, selectedAnswer];
     });
-  }
+  },
+  []);
+
+  const handleSkipAnswer = useCallback(
+    () => handleSelectAnswer(null),
+    [handleSelectAnswer]
+  );
 
   if (quizIsComplete) {
     return (
       <div id="summary">
         <img src={quizCompleteImg} alt="Trophy icon" />
-        <h2>Quiz Completed</h2>
+        <h2>Quiz Completed!</h2>
       </div>
     );
   }
 
-  const shuffledAnswers = [...QUESTIONS[activeQuestionsIndex].answers];
+  const shuffledAnswers = [...QUESTIONS[activeQuestionIndex].answers];
   shuffledAnswers.sort(() => Math.random() - 0.5);
 
   return (
     <div id="quiz">
       <div id="question">
-        <h2>{QUESTIONS[activeQuestionsIndex].text}</h2>
+        <QuestionTimer timeout={10000} onTimeout={handleSkipAnswer} />
+        <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
           {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
