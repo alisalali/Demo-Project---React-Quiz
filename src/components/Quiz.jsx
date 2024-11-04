6be56729f11f2249aaa9369e5d3eab5501c02421
alisalali <alis.alali@hotmail.com>
2024-11-04 23:49:38 +0300

    04-shuffling-answers

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,10 +1,11 @@
-import { useRef, useState } from "react";
+import { useState } from "react";
 import QUESTIONS from "../questions";
+import quizCompleteImg from "../assets/quiz-complete.png";
 
 export default function () {
   const [userAnswers, setUserAnswers] = useState([]);
-
   const activeQuestionsIndex = userAnswers.length;
+  const quizIsComplete = activeQuestionsIndex === QUESTIONS.length;
 
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswers) => {
@@ -12,12 +13,24 @@ export default function () {
     });
   }
 
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizCompleteImg} alt="Trophy icon" />
+        <h2>Quiz Completed</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTIONS[activeQuestionsIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id="quiz">
       <div id="question">
         <h2>{QUESTIONS[activeQuestionsIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionsIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
